4f90c083e3a7a0ce9997cc919e80b068c29546ae
SreeCharan153 <223j1a4632@raghuinstech.com>
2025-08-03 13:50:55 +0530

    feat: implement account management features in API; add account creation, deposit, withdrawal, and update functionalities

--- API.py
+++ API.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.responses import FileResponse, JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Depends
+from fastapi.responses import  JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import APIRouter
 from ATM import ATM
+from typing import Optional
 
 app = FastAPI()
+atm = ATM()
 @app.get("/{pas}")
 def read_root(pas: str):
-    atm = ATM()
     if atm.password_check(pas):
         return {"message": "Password is correct"}
     else:
@@ -16,3 +16,27 @@ def read_root(pas: str):
             status_code=400,
             content={"detail": "Incorrect password"}
         )
+
+@app.get("/create/")
+def create_account(h: str, pin: str, mobileno: str, gmail: str):
+    return {"message": atm.create(h.strip(), pin.strip(), mobileno.strip(), gmail.strip())}
+
+@app.get("/deposit/")
+def deposit_amount(h: str, amount: int, pin:Optional[int]=None ):
+    return {"message": atm.deposit(h.strip(), amount,pin)}
+
+@app.get("/withdraw/")
+def withdraw_amount(h: str, amount: int ,pin: Optional[int] = None):
+    return {"message": atm.withdraw(h,amount, pin)}
+
+@app.get("/enenquiry/")
+def enquiry(h: str,pin: int):
+    return {"message": atm.enquiry(h,pin)}
+
+@app.get("/update-mobile/")
+def update_mobile(h: str, nmobile: str, omobile: str):
+    return {"message": atm.mobile(h, nmobile, omobile)}
+
+@app.get("/update-email/")
+def update_email(h: str, nemail: str, oemail: str):
+    return {"message": atm.email(h, nemail, oemail)}
--- ATM.py
+++ ATM.py
@@ -1,6 +1,11 @@
 import random
 import string
+
 class ATM:
+    def __init__(self) -> None:
+        pass
+    
+    
     def ac(self):
         with open("Accounts List.txt") as a:
             l=a.readlines()[-1].strip()
@@ -10,16 +15,12 @@ class ATM:
             return l
         
 
-    def create(self):
+    def create(self,holder,pin,mobileno,gmail):
         try:
-            holder=input("ENTER ACCOUNT HOLDER NAME:  ")
-            pin=input("CREATE NEW PIN: ")
             if len(pin)!=4 or not pin.isdigit():
                 raise KeyError('PIN MUST BE 4 DIGITS')
-            mobileno=input("ENTER MOBILE NUMBER:")
             if len(mobileno)!=10 or not mobileno.isdigit():
                 raise KeyError('INVALIED MOBILE NUMBER')
-            gmail=input("ENTER GMAIL:")
             if '@' not in gmail:
                 raise KeyError('INVALIED EMAIL ID')
             b=self.ac()
@@ -30,10 +31,8 @@ class ATM:
             with open(file) as h,open('Accounts List.txt','a') as acc:
                 l=h.readlines()
                 acc.writelines(holder+':'+b+'\n')
-        except FileExistsError:
-            print("*"*5,'HOLDER ALREADY EXISTS PLEASE CHOOSE ANOTHER NAME','*'*5)
         except KeyError as a:
-            print('*'*5,a,'*'*5)
+            return(a)
         else:
             l[0]='ACCOUNT HOLDER:'+holder+'\n'
             l[1]='PIN:'+pin+'\n'
@@ -41,102 +40,85 @@ class ATM:
             l[3]='EMAIL ID:'+gmail+'\n'
             with open(file,'w+') as h:
                 h.writelines(l)
-            print("ACCOUNT CREATED with",l[4])
-            print("YOUR ACCOUNT NUMNBER IS",b)
+            return("ACCOUNT CREATED with",l[4],"\nYOUR ACCOUNT NUMNBER IS",b)
 
 
-    def deposit(self):
-        holder=input("ENTER ACCOUNT NUMBER:  ")
-        state=self.check(h=holder)
-        if state==True:
+    def deposit(self,holder, amount, pin):
+        state,m=self.check(h=holder, pin=pin)
+        if state :
             file = f"./Accounts/{holder}.txt"
-            with open(file) as h:
-                l=h.readlines()
+            with open(file) as f:
+                l=f.readlines()
             self.balance=int(l[4].split(':')[1].strip())
             if self.balance<0:
-                print("Balance is negative, please deposit first.")
-                return
-            while True:
-                try:
-                    amount=int(input("Enter the amount to deposit: "))
-                    if amount <= 0:
-                        raise ValueError("Amount must be greater than zero.")
-                except ValueError as e:
-                    print('*'*5, e, '*'*5)
-                else:
-                    break
+                return "Balance is negative, please deposit first."
+            if amount <= 0:
+                return "Amount must be greater than zero."
             self.balance=self.balance+amount
             l[4]='BALANCE:'+str(self.balance)+'\n'
-            with open(file,'w+') as h:
-                h.writelines(l)
-            print(f"Transaction successful! New Balance: ₹{self.balance}")
-        elif state==False:
-            print('*'*5,"WRONG PASSWORD",'*'*5)
+            with open(file,'w+') as f:
+                f.writelines(l)
+            return(f"Transaction successful! New Balance: ₹{self.balance}")
+        else:
+            return m
 
 
-    def withdraw(self):
-        holder=input("ENTER ACCOUNT NUMBER:")
-        state=self.check(h=holder)
+    def withdraw(self,holder,amount, pin):
+        state,m=self.check(h=holder, pin=pin)
         if state==True:
             file = f"./Accounts/{holder}.txt"
             with open(file) as h:
                 l=h.readlines()
             self.balance=int(l[4].split(':')[1].strip())
             if self.balance<=0:
-                print("Balance is negative or Zero, please deposit first.")
-                return
-            try:
-                amount=int(input("Enter the amount to withdraw:"))
-            except ValueError:
-                print('*'*5, "Invalid input. Please enter a numeric amount.", '*'*5)
+                return("Balance is negative or Zero, please deposit first.")
             else:
                 if self.balance<amount:
-                    print("Insufficient Balance")
+                    return("Insufficient Balance")
                 elif amount <= 0:
-                    print('*'*5, "Amount must be greater than zero.", '*'*5)
+                    return("Amount must be greater than zero.")
                 else:
                     self.balance=self.balance-amount
                     l[4]='BALANCE:'+str(self.balance)+'\n'
                     with open(file,'w+') as h:
                         h.writelines(l)
-                    print(f"Transaction successful! New Balance: ₹{self.balance}")
+                    return(f"Transaction successful! New Balance: ₹{self.balance}")
         else:
-            print('*'*5,"WRONG PASSWORD",'*'*5)
+            return(m)
 
 
-    def enquiry(self):
-        holder=input("ENTER ACCOUNT NUMBER:")
-        state=self.check(h=holder)
+    def enquiry(self,holder,pin):
+        state,m=self.check(h=holder,pin=pin)
         if state==True:
             file = f"./Accounts/{holder}.txt"
             with open(file,'r') as h:
                 l=h.readlines()[4]
-            print("ACCOUNT"+l.strip())
+            return("ACCOUNT"+l.strip())
         else:
-            print('*'*5,"WRONG PASSWORD",'*'*5)
+            return m
 
 
-    def check(self,h):
+    def check(self,h, pin=None):
         file = f"./Accounts/{h}.txt"
         try:
             with open(file) as h:
                 l=h.readlines()[1]
         except:
-            print('*'*5,"HOLDER NOT FOUND",'*'*5)
-            return False
+            return (False,"HOLDER NOT FOUND")
         else:
             l=l.split(':')[1]
-            for _ in range(3):
-                try:
-                    pin=int(input('ENTER YOUR PIN:'))
-                    if(pin==int(l)):
-                        return True
-                except ValueError:
-                    print("Invalid input. Please enter a numeric PIN.")
-        return False
+            try:
+                if pin is None:
+                    return (False,"PIN NOT PROVIDED")
+                elif(pin==int(l)):
+                    return (True,"PIN MATCHED")
+                else:
+                    return (False,"WRONG PIN")
+            except ValueError:
+                return(False,"Invalid input. Please enter a numeric PIN.")
     
 
-    def update(self):
+    '''def update(self):
         holder=input("ENTER ACCOUNT NUMBER:  ")
         state=self.check(h=holder)
         if state==True:
@@ -153,61 +135,51 @@ class ATM:
                 case 1:
                     self.mobile(h=holder)
                 case 2:
-                    self.email(h=holder)
+                    self.email(h=holder)'''
 
 
-    def mobile(self,h):
-        while True:
-            try:
-                nmobile = input("ENTER NEW MOBILE NUMBER: ")
-                omobile=input("ENTER OLD MOBILE NUMBER: ")
-                if len(nmobile) != 10 or not nmobile.isdigit() or len(omobile) != 10 or not omobile.isdigit():
-                    raise ValueError("Invalid mobile number. It must be 10 digits.")
-                elif nmobile == omobile:
-                    raise ValueError("New mobile number cannot be the same as the old one.")
-                break
-            except ValueError as e:
-                print('*' * 5, e, '*' * 5)
-        c= self.captcha()
+    def mobile(self,h,nmobile,omobile):
+        try:
+            if len(nmobile) != 10 or not nmobile.isdigit() or len(omobile) != 10 or not omobile.isdigit():
+                raise ValueError("Invalid mobile number. It must be 10 digits.")
+            elif nmobile == omobile:
+                raise ValueError("New mobile number cannot be the same as the old one.")
+        except ValueError as e:
+            return(e)
+        #c= self.captcha()
         file = f"./Accounts/{h}.txt"
         with open(file,'r') as h:
             l=h.readlines()
         l1=l[2].split(':')[1].strip()
-        if l1==omobile.strip() and c:
+        if l1==omobile.strip() :
             l[2]='MOBILE NO:'+nmobile+'\n'
             with open(file,'w') as h:
                 h.writelines(l)
-            print("MOBILE NUMBER UPDATED TO",nmobile)
+            return(f"MOBILE NUMBER UPDATED TO {nmobile}")
         else:
-            print('*'*5,'ACCESS DENIED','*'*5)
-            print('*'*5,'MOBILE NUMBER OR CAPTCHA DOSE NOT MATCH','*'*5)
+            return(f'ACCESS DENIED\nMOBILE NUMBER OR CAPTCHA DOSE NOT MATCH')
 
 
-    def email(self,h):
-        while True:
-            try:
-                nemail=input("ENTER NEW EMAIL ID: ").lower().strip()
-                oemail=input("ENTER OLD EMAIL ID: ").lower().strip()
-                if '@' not in nemail or '@' not in oemail:
-                    raise ValueError("Invalid email format. Please include '@'.")
-                elif nemail == oemail:
-                    raise ValueError("New email cannot be the same as the old one.")
-                break
-            except ValueError as e:
-                print('*' * 5, e, '*' * 5)
-        c=self.captcha()
+    def email(self,h,nemail,oemail):
+        try:
+            if '@' not in nemail or '@' not in oemail:
+                raise ValueError("Invalid email format. Please include '@'.")
+            elif nemail == oemail:
+                raise ValueError("New email cannot be the same as the old one.")
+        except ValueError as e:
+            return(e)
+        #c=self.captcha()
         file = f"./Accounts/{h}.txt"
         with open(file,'r') as h:
             l=h.readlines()
         l1=l[3].split(':')[1].strip().lower()
-        if l1==oemail and c:
+        if l1==oemail:
             l[3]='EMAIL ID:'+nemail+'\n'
             with open(file,'w') as h:
                 h.writelines(l)
-            print("GMAIL UPDATED TO",nemail)
+            return("GMAIL UPDATED TO",nemail)
         else:
-             print('*' * 5, 'ACCESS DENIED', '*' * 5)
-             print('*' * 5, 'EMAILID OR CAPTCHA DOSE NOT MATCH', '*' * 5)
+            return(f'ACCESS DENIED\nEMAILID OR CAPTCHA DOSE NOT MATCH')
 
 
     def password_check(self,pw):
